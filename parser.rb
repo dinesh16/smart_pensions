7c9446d19d05b292b3461cb2e423711bfab379bb
Dinesh Anthony <anthony.dinesh16@gmail.com>
2019-05-29 10:32:23 +0100

    add parser to accept command line args

---
+++ parser.rb
@@ -0,0 +1,15 @@
+require 'pry'
+require 'terminal-table'
+require_relative './lib/data_processor'
+require_relative './lib/data_io_service'
+
+class Parser
+  attr_reader :data_io_service
+  def initialize
+    @data_io_service = DataIoService.new(ARGV.first)
+  end
+end
+
+data_io_service = Parser.new.data_io_service
+data_io_service.display_page_view
+data_io_service.uniq_display_page_view
